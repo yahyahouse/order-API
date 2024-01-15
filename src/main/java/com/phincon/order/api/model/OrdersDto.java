7560f3350f63339803e3df3a99a9325c0952564d
yahya wijaya <yahyawijaya082@gmail.com>
2024-01-15 17:04:26 +0700

    feat : add getter setter manual

--- src/main/java/com/phincon/order/api/model/OrdersDto.java
+++ src/main/java/com/phincon/order/api/model/OrdersDto.java
@@ -1,11 +1,49 @@
 package com.phincon.order.api.model;
 
-import lombok.Data;
-
-@Data
 public class OrdersDto {
     String id;
     String productId;
     Long price;
     String actionId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getActionId() {
+        return actionId;
+    }
+
+    public void setActionId(String actionId) {
+        this.actionId = actionId;
+    }
+
+    public OrdersDto() {
+    }
+    public OrdersDto(String id, String productId, Long price, String actionId) {
+        this.id = id;
+        this.productId = productId;
+        this.price = price;
+        this.actionId = actionId;
+    }
 }
