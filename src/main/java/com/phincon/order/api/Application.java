e3746ed1d71ed2e495e12a5b994c014794bc430f
yahya wijaya <yahyawijaya082@gmail.com>
2024-01-15 14:08:26 +0700

    feat : add swagger

--- src/main/java/com/phincon/order/api/Application.java
+++ src/main/java/com/phincon/order/api/Application.java
@@ -1,9 +1,12 @@
 package com.phincon.order.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Order API", version = "1.0", description = "Documentation Order API v1.0"))
 public class Application {
 
 	public static void main(String[] args) {
